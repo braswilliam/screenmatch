871395f8633b07feef63b038faa64f3611cf11be
William B. Pereira <braswilliam@gmail.com>
2024-03-08 01:00:00 -0300

    Nessa aula, você aprendeu sobre:

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,16 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "Duna parte dois";
-        meuFilme.anoDeLancamento = 2024;
+        meuFilme.setNome("Duna parte dois");
+        meuFilme.setAnoDeLancamento(2024);
+        meuFilme.setDuracaoEmMinutos(180);
+
 
 
         meuFilme.avalia(10.0);
@@ -12,8 +18,35 @@ public class Principal {
         meuFilme.avalia(9.8);
 
         meuFilme.exibeFichaTecnica();
-
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
         System.out.printf("%.2f%n", meuFilme.pegaMedia());
+
+        System.out.println("-----------------------");
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setMinutosPorEpisodio(50);
+        lost.setEpisodiosPortemporada(10);
+        System.out.println("Duração para maratonar " + lost.getNome() + ":" + lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+
+
+        Filme outroFilme = new Filme();
+
+        outroFilme.setNome("Avatar - O Caminho da Água");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        calculadora.incluirNaCalculadora(outroFilme);
+        calculadora.incluirNaCalculadora(meuFilme);
+        calculadora.incluirNaCalculadora(lost);
+
+        //pega o tempo total do filme:
+        System.out.println(calculadora.getTempoTotal());
 
 
 
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,71 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+    //Atributos:
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + getNome());
+        System.out.println("Ano de lançamento: " + getAnoDeLancamento());
+        System.out.println("Soma das notas: " + getSomaDasAvaliacoes());
+        System.out.println("Total de avaliações: " + getTotalDeAvaliacoes());
+    }
+
+    public void avalia(double nota) {
+        this.somaDasAvaliacoes += nota;
+        this.totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        System.out.print("Media das Avaliacoes: ");
+        return this.somaDasAvaliacoes / this.totalDeAvaliacoes;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public double getSomaDasAvaliacoes() {
+        return somaDasAvaliacoes;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+}

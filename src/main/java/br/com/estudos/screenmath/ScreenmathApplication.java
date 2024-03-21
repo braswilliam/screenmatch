340d744315354158413008c9afc9d5bededceccd
William B. Pereira <braswilliam@gmail.com>
2024-03-20 23:13:17 -0300

    Project started

---
+++ src/main/java/br/com/estudos/screenmath/ScreenmathApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.screenmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudos/screenmath/ScreenmathApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.screenmath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
